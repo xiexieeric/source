4ee8440cda973e853cb6a6ce5b14d1b35d87fa88
Erik Song <es2fq@virginia.edu>
2017-10-17 02:17:16 -0400

    removed solutions

--- week5/chatApp/server.js
+++ week5/chatApp/server.js
@@ -12,7 +12,9 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function (socket) {
-    socket.on('new chat', function (data) {
-        socket.broadcast.emit('new chat', data);
-    });
+    ///////////////////////////////////////////////////
+    //
+    // How should the server handle new chats?
+    //
+    ///////////////////////////////////////////////////
 });
